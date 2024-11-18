c8b56af63e50912bc8a63d8c4ba0cb0a07b0fd2e
Tiago <tiago@trattecinformatica.com.br>
2024-11-17 21:07:23 -0300

    Produtos Config: models.py

--- projeto_teste/produtos/models.py
+++ projeto_teste/produtos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class produto(models.Model):
+    nome_produto = models.CharField(max_length=100)
+    preco_produto = models.DecimalField(max_digits=8, decimal_places=2)
+    descricao_produto = models.TextField()
